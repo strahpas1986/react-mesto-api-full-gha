e65ef1cc2f2363d15f1a3ec8c0f4a5ab63338d69
strahpas <strahpas1986@yandex.ru>
2023-05-28 21:50:07 +0300

    fix cors2

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,8 @@ const rootRouter = require('./routes/index');
 const limiter = require('./middlewares/limiter');
 const errors = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
-// const cors = require('cors');
+// const cors = require('./middlewares/cors');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 const DATABASE = process.env.DATABASE || 'mongodb://localhost:27017/mestodb';
@@ -25,21 +25,21 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 app.use(limiter);
-// app.use(cors({
-//   origin: [
-//     'http://mesto-sp.nomoredomains.monster',
-//     'https://mesto-sp.nomoredomains.monster',
-//     'http://api.mesto-sp.nomoredomains.monster',
-//     'https://api.mesto-sp.nomoredomains.monster',
-//     'http://84.201.142.51',
-//     'https://84.201.142.51',
-//     'http://localhost:3000',
-//     'http://localhost:3001',
-//   ],
-//   method: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-//   credentials: true,
-// }));
-app.use(cors);
+app.use(cors({
+  origin: [
+    'http://mesto-sp.nomoredomains.monster',
+    'https://mesto-sp.nomoredomains.monster',
+    'http://api.mesto-sp.nomoredomains.monster',
+    'https://api.mesto-sp.nomoredomains.monster',
+    'http://84.201.142.51',
+    'https://84.201.142.51',
+    'http://localhost:3000',
+    'http://localhost:3001',
+  ],
+  method: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  credentials: true,
+}));
+// app.use(cors);
 
 app.use(requestLogger);
 
